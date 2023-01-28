223c0927578a7e63f201ddc718de8b6dd941c02d
郑凯杰 <zhengkaijie@ghbank.com.cn>
2023-01-28 20:50:44 +0800

    upd: canvas默认字体

--- libs/configCanvas/canvas.js
+++ libs/configCanvas/canvas.js
@@ -324,7 +324,7 @@ const drawText = function (ctx, text, font, x, y, maxWidth = 999) {
   // }
   // let canvasFont = `${bold ? "bold" : "normal"} ${style}`;
   ctx.font = "normal 400 15px Arial, sans-serif"; // 重置
-  let canvasFont = `normal ${bold ? "bold" : "normal"} ${currentFontSize}px ${fontFamily ? fontFamily : "sans-serif"} ${style}`;
+  let canvasFont = `normal ${bold ? "bold" : "normal"} ${currentFontSize}px ${fontFamily ? fontFamily : "Arial, sans-serif"} ${style}`;
   ctx.font = canvasFont.replace(/(^\s*)|(\s*$)/g, "");
   console.log("ctx font:", ctx.font);
   // if (textAlign) {
